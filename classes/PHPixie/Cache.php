d00fa700dfdb13b31821ab6d3dc88f6e6d85a6ba
Grundik <grundik@ololo.cc>
2014-03-14 18:06:45 +0400

    Small typo fix

--- classes/PHPixie/Cache.php
+++ classes/PHPixie/Cache.php
@@ -11,7 +11,7 @@ namespace PHPixie;
  * </code>
  * to your requirement definition. Or download it from
  * https://github.com/dracony/PHPixie-cache
- * 
+ *
  * To enable it add it to your Pixie class' modules array:
  * <code>
  * 		protected $modules = array(
@@ -34,44 +34,44 @@ class Cache {
 	 * @var \PHPixie\Pixie
 	 */
 	public $pixie;
-	
+
 	/**
 	 * An associative array of cache instances
-	 * @var array   
+	 * @var array
 	 */
 	private $_instances=array();
 
 	/**
 	 * Initializes the cache module
-	 * 
+	 *
 	 * @param \PHPixie\Pixie $pixie Pixie dependency container
 	 */
 	public function __construct($pixie) {
 		$this->pixie = $pixie;
 	}
-	
+
 	/**
 	 * Gets an instance of a cache configuration
-	 * 
+	 *
 	 * @param string  $config Configuration name.
 	 *                        Defaults to  'default'.
 	 * @return \PHPixie\Cache\Abstract Driver implementation of Abstact_Cache
 	 */
 	public function instance($config='default'){
-		if (!isset($this->_isntances[$config])) {
+		if (!isset($this->_instances[$config])) {
 			$driver = $this->pixie->config->get("cache.{$config}.driver");
 			$driver="\\PHPixie\\Cache\\{$driver}";
-			$this->_isntances[$config] = new $driver($this->pixie, $config);
+			$this->_instances[$config] = new $driver($this->pixie, $config);
 		}
-		return $this->_isntances[$config];
+		return $this->_instances[$config];
 	}
-	
+
 	/**
 	 * Caches a value for the duration of the specified lifetime.
-	 * 
+	 *
 	 * @param  string  $key       Name to store the object under
 	 * @param  mixed   $value     Object to store
-	 * @param  int     $lifetime  Validity time for this object in seconds. 
+	 * @param  int     $lifetime  Validity time for this object in seconds.
 	 * 							  Default's to the value specified in config, or to 3600
 	 *                            if it was not specified.
 	 * @param  string  $config    Cache configuration to use
@@ -80,10 +80,10 @@ class Cache {
 	public function set($key, $value, $lifetime = null, $config = 'default') {
 		$this->instance($config)->set($key,$value,$lifetime);
 	}
-	
+
 	/**
 	 * Gets a stored cache value of the cache specified by $config.
-	 * 
+	 *
 	 * @param  string  $key       Name of the object to retrieve
 	 * @param  mixed   $default   Default value to return if the object is not found
 	 * @param  string  $config    Cache configuration to use
@@ -93,10 +93,10 @@ class Cache {
 	public function get($key, $default = null, $config = 'default') {
 		return $this->instance($config)->get($key,$default);
 	}
-	
+
 	/**
 	 * Deletes an object from the cache specified by $config.
-	 * 
+	 *
 	 * @param  string  $key       Name of the object to remove
 	 * @param  string  $config    Cache configuration to use
 	 * @see \PHPixie\Cache\Abstract::delete()
@@ -104,25 +104,25 @@ class Cache {
 	public function delete($key, $config = 'default') {
 		$this->instance($config)->delete($key);
 	}
-	
+
 	/**
 	 * Clears cache specified by $condfig
-	 * 
+	 *
 	 * @param  string  $config    Cache configuration to use
 	 * @see \PHPixie\Cache\Abstract::clear()
 	 */
 	public function clear($config = 'default') {
 		$this->instance($config)->clear();
 	}
-	
+
 	/**
 	 * Checks and removes expired objects from cache specified by $config
-	 * 
+	 *
 	 * @param  string  $config    Cache configuration to use
 	 * @see \PHPixie\Cache\Abstract::garbage_collect()
 	 */
-	public function garbage_collect($config = 'default') { 
+	public function garbage_collect($config = 'default') {
 		$this->instance($config)->garbage_collect();
 	}
-	
+
 }
